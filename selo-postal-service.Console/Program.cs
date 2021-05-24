8f0d2a7d6bc3640c6fc19ba62a8866d543ab0e7c
Elias Santos Garcia <elias.garcia@solutis.com.br>
2021-05-24 17:15:26 -0300

    Estrutura pŕevia do projeto criada

---
+++ selo-postal-service.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace selo_postal_service.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ selo-postal-service.Dados/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace selo_postal_service.Dados
+{
+    public class Class1
+    {
+    }
+}
