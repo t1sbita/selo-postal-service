84463810702bc91038e5cea843347563545853f0
Elias Santos Garcia <elias.garcia@solutis.com.br>
2021-05-25 07:18:12 -0300

    Adicionada propriedade NumeroCasa

--- selo-postal-service.Dados/Etiquetas.cs
+++ selo-postal-service.Dados/Etiquetas.cs
@@ -6,6 +6,7 @@ namespace selo_postal_service.Dados
     {
         public string Nome { get; set; }
         public string Endereco { get; set; }
+        public string NumeroCasa { get; set; }
         public string CodigoPostal { get; set; }
         public string Bairro { get; set; }
         public string Cidade { get; set; }
@@ -15,10 +16,11 @@ namespace selo_postal_service.Dados
         {
 
         }
-        public Etiquetas(string nome, string endereco, string codigoPostal, string bairro, string cidade, string estado)
+        public Etiquetas(string nome, string endereco, string numeroCasa, string codigoPostal, string bairro, string cidade, string estado)
         {
             this.Nome = nome;
             this.Endereco = endereco;
+            this.NumeroCasa = numeroCasa;
             this.CodigoPostal = codigoPostal;
             this.Bairro = bairro;
             this.Cidade = cidade;
