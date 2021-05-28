83ea8ef6d03b269ffc75de36a125608467487565
Elias Santos Garcia <elias.garcia@solutis.com.br>
2021-05-28 14:51:52 -0300

    Modificações solicitadas realizadas

--- selo-postal-service.Application/ImprimeNaTela.cs
+++ selo-postal-service.Application/ImprimeNaTela.cs
@@ -6,14 +6,16 @@ namespace selo_postal_service.Application
     {
         public static void ImprimirTela()
         {
-            string cidade, estado, codigoPostal, paginaDesejada, qtdPorPagina;
-            Console.Write("Escreva a cidade que você deseja pesquisar: ");
-            cidade = Console.ReadLine();
-            Console.Write("Escreva o estado que você deseja pesquisar: ");
-            estado = Console.ReadLine();
-            Console.Write("Escreva o código postal que você deseja pesquisar: ");
-            codigoPostal = Console.ReadLine();
+            string cidade = String.Empty;
+            string estado = String.Empty;
+            string codigoPostal = String.Empty;
+            int paginaDesejada = 0;
+            int qtdPorPagina = 0;
 
+            Console.Write("Escreva a cidade que você deseja pesquisar: ");
+            Console.Write("Escreva o estado que você deseja pesquisar: ");
+            Console.Write("Escreva o código postal que você deseja pesquisar: ");
+            
             System.Console.WriteLine($"\nIremos pesquisar usando os seguintes filtros:");
             System.Console.WriteLine($"Cidade: {cidade}");
             System.Console.WriteLine($"Estado: {estado}");
@@ -23,10 +25,8 @@ namespace selo_postal_service.Application
             Console.WriteLine("Obs.: digitar valores inválidos ou deixar em branco irá setar os valores no padrão");
             Console.WriteLine("Padrão: Página 1 e 10 itens por página! \n");
             Console.Write("Qual a página desejada: ");
-            paginaDesejada = Console.ReadLine();
             Console.Write("Quantos itens por página: ");
-            qtdPorPagina = Console.ReadLine();
-
+            
             ValidarEntradas.Validar(cidade, estado, codigoPostal, paginaDesejada, qtdPorPagina);
         }
     }
--- selo-postal-service.Application/ValidarEntradas.cs
+++ selo-postal-service.Application/ValidarEntradas.cs
@@ -7,15 +7,15 @@ namespace selo_postal_service.Application
 {
     public class ValidarEntradas
     {
-        internal static void Validar(string cidade, string estado, string codigoPostal, string paginaDesejada, string qtdPorPagina)
+        internal static void Validar(string cidade, string estado, string codigoPostal, int paginaDesejada, int qtdPorPagina)
         {
-            if (paginaDesejada.Equals("") || !paginaDesejada.All(char.IsDigit))
+            if(paginaDesejada == 0)
             {
-                paginaDesejada = "1";
+                paginaDesejada = 1;
             }
-            if (qtdPorPagina.Equals("") || !qtdPorPagina.All(char.IsDigit))
+            if(qtdPorPagina == 0)
             {
-                qtdPorPagina = "10";
+                qtdPorPagina = 10;
             }
             
             System.Console.WriteLine($"\nIremos retornar a página: {paginaDesejada}, com {qtdPorPagina} itens por página \n");
