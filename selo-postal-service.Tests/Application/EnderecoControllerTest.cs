2461b067ee396bad4984f26fce09d5946d0bbd26
Elias Santos Garcia <elias.garcia@solutis.com.br>
2021-06-04 10:20:58 -0300

    Testes EnderecoController e EnderecoRepository implantados

--- selo-postal-service.Tests/Application/EnderecoControllerTest.cs
+++ selo-postal-service.Tests/Application/EnderecoControllerTest.cs
@@ -3,21 +3,36 @@ using System;
 
 using selo_postal_service.Application.Controller;
 using selo_postal_service.Core.Domain.DTO;
-using selo_postal_service.Core.Domain.Entities;
-
-
+using selo_postal_service.Core.Services;
+using selo_postal_service.Data.Repository;
+using System.IO;
 namespace selo_postal_service.Tests.Application
 {
     [TestFixture]
     public class EnderecoControllerTest
     {
-        
-        
 
-        [TestCase]
-        public void TestaSeMetodoEstaCorreto()
+        EnderecoController enderecoController = new EnderecoController(new EnderecoService(new EnderecoRepository()), new ArquivoService(new ArquivoRepository()), new QrCodeService(new QrCodeRepository()));
+        
+        [SetUp]
+        public void SetUp()
         {
-            Assert.Pass();
+            SearchEnderecoQueryItem search = new SearchEnderecoQueryItem(null, "Bahia", null);
+
+
         }
+
+        [TestCase("Salvador", "Bahia", null, 1, 5, @"..\..\..\TsvTests\TsvTestCidadeEstado.tsv")]
+        [TestCase(null, null, "CD548J2547", null, 11, @"..\..\..\TsvTests\TsvTestCodigo.tsv")]
+        public void TestaSeTSVEstaCorreto(string cidade, string estado, string codigoPostal, Nullable<int> number, Nullable<int> limit, string expected)
+        {
+            string result = enderecoController.Search(cidade, estado, codigoPostal, number, limit);
+            FileAssert.AreEqual(result, expected);
+            File.Delete(result);
+            Directory.Delete(@"..\..\..\QRCode", true);
+            Directory.Delete(@"..\..\..\TSV", true);
+
+        }
+
     }
 }
---
+++ selo-postal-service.Tests/Repository/EnderecoRepositoryTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+
+using selo_postal_service.Data.Repository;
+using selo_postal_service.Core.Exceptions;
+using selo_postal_service.Core.Domain.DTO;
+using selo_postal_service.Core.Domain.Entities;
+
+namespace selo_postal_service.Tests.Repository
+{
+    [TestFixture]
+    class EnderecoRepositoryTest
+    {
+        EnderecoRepository enderecoRepository;
+        SearchEnderecoQueryItem searchEnderecoQueryItem;
+        PageRequest pageRequest;
+
+        [SetUp]
+        public void SetUp()
+        {
+            enderecoRepository = new EnderecoRepository();
+            searchEnderecoQueryItem = new SearchEnderecoQueryItem(null, null, null);
+            pageRequest = PageRequest.First();
+        }
+
+        [TestCase]
+        public void TestaLancamentoException()
+        {
+            var teste = searchEnderecoQueryItem;
+            var pagina = pageRequest;
+            Assert.Throws<NotFoundException>(
+                () => enderecoRepository.GetByParamets(searchEnderecoQueryItem, pageRequest)
+                );
+        }
+
+    }
+}
